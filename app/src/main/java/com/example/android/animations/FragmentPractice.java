608e02581b8d18e55044624a273eadd9f2350279
shreyas <shreyasanother@gmail.com>
2019-08-11 00:29:34 +0530

    Hamburger menu

--- app/src/main/java/com/example/android/animations/FragmentPractice.java
+++ app/src/main/java/com/example/android/animations/FragmentPractice.java
@@ -5,6 +5,9 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -14,6 +17,45 @@ public class FragmentPractice extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+
+       // setHasOptionsMenu(true);
+
         return inflater.inflate(R.layout.fragment_practice,container, false);
+
+
     }
+
+//    @Override
+//    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+//        // Inflate the fragment menu items.
+//        inflater.inflate(R.menu.menu_top, menu);
+//        super.onCreateOptionsMenu(menu, inflater);
+//    }
+//
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        switch(item.getItemId())
+//        {
+//            case R.id.top_star:
+//            {
+//                break;
+//            }
+//
+//            case R.id.top_notifications:
+//            {
+//                break;
+//            }
+//
+//            case R.id.top_group:
+//            {
+//                break;
+//            }
+//        }
+//
+//
+//            return true;
+//    }
 }
+
+
--- app/src/main/java/com/example/android/animations/MainActivity.java
+++ app/src/main/java/com/example/android/animations/MainActivity.java
@@ -2,21 +2,43 @@ package com.example.android.animations;
 
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.MenuItem;
+import android.widget.Toolbar;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
+    DrawerLayout drawerLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        setTitle("");
+
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
         getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new FragmentPractice()).commit();
 
+
+
+        android.support.v7.widget.Toolbar toolbar = findViewById(R.id.hamburgerToolbar);
+        setSupportActionBar(toolbar);
+
+        drawerLayout = findViewById(R.id.drawerLayout);
+        NavigationView navigationView = findViewById(R.id.hamburger_nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar,
+                R.string.navigation_drawer_open,R.string.navigation_drawer_close );
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
 
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
@@ -51,4 +73,49 @@ public class MainActivity extends AppCompatActivity {
         });
 }
 
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) { // hamburger menu
+        Fragment selectedFragment = null;
+        switch (menuItem.getItemId()){
+
+
+
+            case R.id.ham_lesson:
+                selectedFragment  = new FragmentLessons();
+                break;
+            case R.id.ham_allLessons:
+                selectedFragment  = new FragmentAllLessons();
+                break;
+            case R.id.ham_progress:
+                selectedFragment  = new FragmentProgress();
+                break;
+            case R.id.ham_practice:
+                selectedFragment  = new FragmentPractice();
+                break;
+            case R.id.ham_premium:
+                selectedFragment  = new FragmentPremium();
+                break;
+
+
+        }
+
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
+
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(drawerLayout.isDrawerOpen(GravityCompat.START))
+        {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }
+
+        else
+        {
+            super.onBackPressed();
+        }
+    }
 }
